469e5de7b2e49a5ab7dccfa1cc7deda3f64e6616
talented_vicky <victorotubure7@gmail.com>
2024-05-30 19:54:01 +0100

    gettting movie details and adding movies to watched list

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
 const tempWatchedData = [
   {
     imdbID: "tt1375666",
@@ -55,12 +31,30 @@ const api_key = "406a40fb";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState(tempWatchedData);
+  // const [watched, setWatched] = useState(tempWatchedData);
+  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [err, setErr] = useState("");
   const [query, setQuery] = useState("");
-  
-  // const search_title = query; //constantine
+  const [movieId, setMovieId] = useState(null)
+
+  function handleMovieId (id) {
+    setMovieId(selectedId => selectedId === id ? null : id)
+  }
+
+  function handleCloseMovie(){
+    setMovieId(null)
+  }
+
+  function handleAddWatched(movie) {
+    const AddedMov = watched.find(mov => mov.imdbID === movie.imdbID)
+    if(AddedMov){
+      console.log("Movie already added to watched list")
+      return
+    }
+    setWatched(watched => [...watched, movie])
+    setMovieId(null)
+  }
 
   // render logic is basically what gets called when a component first mounts
   // hence you should never set state in render logic (infinite request loop)
@@ -124,15 +118,23 @@ export default function App() {
         <MoviesBox>
           { isLoading && <LoadingData></LoadingData> }
           {  err && <ErrorPage msg={err}></ErrorPage> }
-          { !isLoading && !err && <MovieData movies={movies}></MovieData>}
+          { !isLoading && !err && <MovieData movies={movies} onMovieId={handleMovieId}></MovieData>}
         </MoviesBox>
         
         <MoviesBox>
-          <>
-            <Summary watched={watched}></Summary>
-            <ReadMore></ReadMore>
-            <WatchedData watched={watched}></WatchedData>
-          </>
+          { movieId 
+          ? <MovieDetails id={movieId} 
+              onCloseMovie={handleCloseMovie}
+              onAddWatched={handleAddWatched}
+            >
+            </MovieDetails>
+          :
+            <>
+              <Summary watched={watched}></Summary>
+              {/* <ReadMore></ReadMore> */}
+              <WatchedData watched={watched}></WatchedData>
+            </>
+          }
         </MoviesBox>
       </MovieBody>
     </>
@@ -149,16 +151,6 @@ function MovieNav({children}) {
     </nav>
   )
 }
-
-// function MovieNav({movies}) {
-//   return (
-//     <nav className="nav-bar">
-//         <Logo></Logo>
-//         <Search></Search>
-//         <SearchResult movies={movies}></SearchResult>
-//     </nav>
-//   )
-// }
 
 
 // movie_navigation_component
@@ -217,11 +209,11 @@ function MoviesBox({children}) {
 }
 // movie_body_component
 // ******
-function MovieData({movies}) {
+function MovieData({movies, onMovieId}) {
   return (
-    <ul className="list">
+    <ul className="list list-movies">
       {movies?.map((movie) => (
-        <MovieInfo movie={movie} key={movie.imdbID}></MovieInfo>
+        <MovieInfo movie={movie} key={movie.imdbID} onMovieId={onMovieId}></MovieInfo>
       ))}
     </ul>
   )
@@ -236,10 +228,70 @@ function ErrorPage({msg}) {
     <span> {msg} </span>
   </div>
 }
-function MovieInfo({movie}) {
+function MovieDetails({id, onCloseMovie, onAddWatched}) {
+  const [movie, setMovie] = useState({})
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { Title, Genre, Plot, Poster, Released, 
+    Runtime, imdbRating, Actors, Director } = movie
+
+  useEffect(() => {
+    const LoadMovieDetails = async () => {
+      try {
+        setIsLoading(true)
+        const data = await fetch(`http://www.omdbapi.com/?apikey=${api_key}&i=${id}`)
+        if(!data.ok){
+          throw new Error("Network Error!")
+        }     
+        
+        const jsonData = await data.json()
+        setMovie(jsonData)
+        
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    LoadMovieDetails()
+  }, [id]) 
+
   return (
-    <li>
-      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+    <div className="details">
+      {
+        isLoading ? <LoadingData></LoadingData>
+        :
+        <>
+          <header>
+            <button className="btn-back" onClick={onCloseMovie}> ⬅</button>
+            <img src={Poster} alt={Title}></img>
+            <div className="details-overview">
+              <h2> {Title} </h2>
+              <p> {Released} ▫ {Runtime}</p>
+              <p> {Genre} </p>
+              <p> {imdbRating}⭐ IMDB Rating`</p>
+            </div>
+          </header>
+
+          <section>
+            <div className="rating">
+              <Star initRate={0} maxRate={10} setExtRating={() => {}}></Star>
+              <button onClick={() => onAddWatched(movie)}> ➕ Add to List </button>
+            </div>
+            <p> {Plot} </p>
+            <p> Actors: {Actors}</p>
+            <p> Directed by: {Director} </p>
+          </section>
+        </>
+      }
+    </div>
+  )
+}
+function MovieInfo({movie, onMovieId}) {
+  return (
+    <li onClick={() => onMovieId(movie.imdbID)}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`}/>
       <h3>{movie.Title}</h3>
         <p>
           <span>🎭</span>
@@ -249,9 +301,9 @@ function MovieInfo({movie}) {
   )
 }
 function Summary({watched}){
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
+  const avgImdbRating = average(watched.map((movie) => Number(movie.imdbRating)));
+  const avgUserRating = average(watched.map((movie) => Number(movie.userRating)));
+  const avgRuntime = average(watched.map((movie) => movie.Runtime.split(" ").at(0)));
   return (
     <div className="summary">
                 <h2>Movies you watched</h2>
@@ -286,7 +338,7 @@ function WatchedData({watched}){
   )
 }
 function WatchedInfo({movie}) {
-  const [rating, setRating] = useState("");
+  // const [rating, setRating] = useState("");
   return (
     <li>
           <img src={movie.Poster} alt={`${movie.Title} poster`} />
@@ -302,14 +354,12 @@ function WatchedInfo({movie}) {
             </p>
             <p>
               <span>⏳</span>
-              <span>{movie.runtime} min</span>
+              <span>{movie.Runtime}</span>
             </p>
           </div>
           <div>
-            {/* <Star setExtRating={setRating}></Star> */}
-            <Star setExtRating={setRating}></Star>
-            <p> you rated this a {rating === "" ? 0 : rating}</p>
-            {/* <Star maxRate={7} initRate={4} noRateIcon={"🕳"} rateIcon={"💫"}></Star> */}
+            {/* <Star setExtRating={setRating}></Star>
+            <p> you rated this a {rating === "" ? 0 : rating}</p> */}
           </div>
         </li>
   )
@@ -334,7 +384,7 @@ Star.propTypes = {
   setExtRating: PropTypes.func
 }
 
-function Star({ maxRate = 5, initRate = 2, noRateIcon="🤍", rateIcon="💖", setExtRating}) {
+function Star({ maxRate = 5, initRate = 2, noRateIcon="🤍", rateIcon="💖", setExtRating={}}) {
   const [rate, setRate] = useState(initRate)
   const [tempRate, setTempRate] = useState("")
   
@@ -361,85 +411,85 @@ function Star({ maxRate = 5, initRate = 2, noRateIcon="🤍", rateIcon="💖", s
   ) 
 }
 
-function ReadMore() {
-  return (
-    <div>
-      <ExpandingText
-        collapsedNumWords={20}
-        expandButtonText="Show more"
-        collapseButtonText="Read Less"
-        buttonColor="#ffff"
-      >
-        Space travel requires some seriously amazing technology and
-        collaboration between countries, private companies, and international
-        space organizations. And while it's not always easy (or cheap), the
-        results are out of this world. Think about the first time humans stepped
-        foot on the moon or when rovers were sent to roam around on Mars.
-      </ExpandingText>
-      <ExpandingText
-        collapsedNumWords={10}
-        expandButtonText="More"
-        collapseButtonText="Less"
-        buttonColor="#ff6622"
-      >
-        Space travel is the ultimate adventure! Imagine soaring past the stars
-        and exploring new worlds. It's the stuff of dreams and science fiction,
-        but believe it or not, space travel is a real thing. Humans and robots
-        are constantly venturing out into the cosmos to uncover its secrets and
-        push the boundaries of what's possible.
-      </ExpandingText>
+// function ReadMore() {
+//   return (
+//     <div>
+//       <ExpandingText
+//         collapsedNumWords={20}
+//         expandButtonText="Show more"
+//         collapseButtonText="Read Less"
+//         buttonColor="#ffff"
+//       >
+//         Space travel requires some seriously amazing technology and
+//         collaboration between countries, private companies, and international
+//         space organizations. And while it's not always easy (or cheap), the
+//         results are out of this world. Think about the first time humans stepped
+//         foot on the moon or when rovers were sent to roam around on Mars.
+//       </ExpandingText>
+//       <ExpandingText
+//         collapsedNumWords={10}
+//         expandButtonText="More"
+//         collapseButtonText="Less"
+//         buttonColor="#ff6622"
+//       >
+//         Space travel is the ultimate adventure! Imagine soaring past the stars
+//         and exploring new worlds. It's the stuff of dreams and science fiction,
+//         but believe it or not, space travel is a real thing. Humans and robots
+//         are constantly venturing out into the cosmos to uncover its secrets and
+//         push the boundaries of what's possible.
+//       </ExpandingText>
 
-      <ExpandingText 
-        expanded={true} className="box"
-      >
-        Space missions have given us incredible insights into our universe and
-        have inspired future generations to keep reaching for the stars. Space
-        travel is a pretty cool thing to think about. Who knows what we'll
-        discover next!
-      </ExpandingText>
-    </div>
-  )
-}
+//       <ExpandingText 
+//         expanded={true} className="box"
+//       >
+//         Space missions have given us incredible insights into our universe and
+//         have inspired future generations to keep reaching for the stars. Space
+//         travel is a pretty cool thing to think about. Who knows what we'll
+//         discover next!
+//       </ExpandingText>
+//     </div>
+//   )
+// }
 
 
-function ExpandingText({collapsedNumWords, expandButtonText, collapseButtonText, buttonColor, expanded, children}) {
-  const [exp, setExp] = useState(true)
-  const [text, setText] = useState(children)
+// function ExpandingText({collapsedNumWords, expandButtonText, collapseButtonText, buttonColor, expanded, children}) {
+//   const [exp, setExp] = useState(true)
+//   const [text, setText] = useState(children)
 
-  const btnStyle = {
-    color: buttonColor,
-    background: "none",
-    border: "none",
-    fontSize: "10px",
-    cursor: "pointer",
-    marginLeft: "3px"
-  }
+//   const btnStyle = {
+//     color: buttonColor,
+//     background: "none",
+//     border: "none",
+//     fontSize: "10px",
+//     cursor: "pointer",
+//     marginLeft: "3px"
+//   }
 
-  function altTextContent(wholeText, limit) {
-    if(exp) {
-      const shortened = wholeText.split(" ").slice(0, limit).join(" ") + "..."
-      setText(shortened);
-    } else {
-      setText(children)
-    }
-  }
+//   function altTextContent(wholeText, limit) {
+//     if(exp) {
+//       const shortened = wholeText.split(" ").slice(0, limit).join(" ") + "..."
+//       setText(shortened);
+//     } else {
+//       setText(children)
+//     }
+//   }
 
-  function toggleBtn () {
-    setExp(!exp)
-  }
+//   function toggleBtn () {
+//     setExp(!exp)
+//   }
   
-  useEffect(() => {
-    altTextContent(children, collapsedNumWords)
-  })
+//   useEffect(() => {
+//     altTextContent(children, collapsedNumWords)
+//   })
 
-  return (    
-    <div >  
-      <span> {expanded ? children : text } </span>
-      <button style={btnStyle} onClick={() => {
-        altTextContent(children, collapsedNumWords)
-        toggleBtn()
-      }}> {exp ? expandButtonText : collapseButtonText} 
-      </button>
-    </div>
-  )
-}
+//   return (    
+//     <div >  
+//       <span> {expanded ? children : text } </span>
+//       <button style={btnStyle} onClick={() => {
+//         altTextContent(children, collapsedNumWords)
+//         toggleBtn()
+//       }}> {exp ? expandButtonText : collapseButtonText} 
+//       </button>
+//     </div>
+//   )
+// }
